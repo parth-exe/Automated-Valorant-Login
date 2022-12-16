f7c46a68fdbc1184e05fd796d0d479e3a32f7b2c
Parth <108059096+parth-exe@users.noreply.github.com>
2022-12-17 00:35:08 +0530

    Updated code with multiple account support
    Once the code is copy pasted into a code editor of python, run command-
    pyinstaller valorant_login.py --onefile in terminal to convert the code to an executable to make the process easier to login.

--- valorant-login.py
+++ valorant-login.py
@@ -2,20 +2,49 @@ import pyautogui as pg
 import os
 import time
 
-os.startfile("C:\Riot Games\VALORANT\Riot Client\RiotClientServices.exe") # This is the default target location of Riot Client. If your target location is different then paste the target by going to the properties of the shortcut and pasting the location.
+username = ""
+password = ""
+def menu():
+    option = int(input("Enter which account to log in to: "))
+    global username
+    global password
+    if option == 1:
+        username = "<account username>"
+        password = "<account password>"
+    elif option == 2:
+        username = "<account username>"
+        password = "<account password>"
+    elif option == 3:
+        username = "<account username>"
+        password = "<account password>"
+    elif option == 4:
+        username = "<account username>"
+        password = "<account password>"
+    #Copy elif code to add more accounts, replace with unique key and display in the prints below from code 31
+        
+    else:
+        print(f"No account exists with the associated key: {option}")
+        exit()
+  
+
+print("Accounts available:")
+print("1. Account name")
+print("2. Account name")
+print("3. Account name")
+# Print as many accounts as you want to display in terminal
+menu()
+os.startfile("C:\Riot Games\VALORANT\Riot Client\RiotClientServices.exe")
 time.sleep(6)
 pg.moveTo(349, 368)
 pg.leftClick()
-pg.typewrite(<your username>)
+pg.typewrite(username)
 time.sleep(1)
 pg.moveTo(315, 423)
 pg.leftClick()
-pg.typewrite(<your password>)
+pg.typewrite(password)
 time.sleep(1)
 pg.moveTo(402, 795)
 pg.leftClick()
 
-#To make this easier, in terminal type pip install pyinstaller.
-#Once the installation of the package is done, in the terminal type the command
-# pyinstaller valorant-login.py --onefile
-# This will create .exe file of this program which can be run without opening a code editor.
+
+
